e31dbc46cb0ece61a856a8ecd4167f6db4ed28ae
绿桶 <1325285734@qq.com>
2025-03-11 19:12:52 +0800

    feat:maven

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import com.learn.service.BookService;
-import com.learn.service.impl.BookServiceImpl;
-
-public class Main {
-    public static void main(String[] args) {
-        BookService bookService = new BookServiceImpl();
-        bookService.save();
-    }
-}
