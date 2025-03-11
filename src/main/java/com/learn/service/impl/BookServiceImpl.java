ea8ef1d1df7782edefcc7642c9e98c46be9822ed
绿桶 <1325285734@qq.com>
2025-03-11 20:12:29 +0800

    feat:DI入门

--- src/main/java/com/learn/service/impl/BookServiceImpl.java
+++ src/main/java/com/learn/service/impl/BookServiceImpl.java
@@ -1,14 +1,17 @@
 package com.learn.service.impl;
 
 import com.learn.dao.BookDao;
-import com.learn.dao.impl.BookDaoImpl;
 import com.learn.service.BookService;
 
 public class BookServiceImpl implements BookService {
-    private BookDao bookDao = new BookDaoImpl();
+    private BookDao bookDao;
 
     public void save() {
         System.out.println("BookServiceImpl save...");
         bookDao.save();
+    }
+
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
     }
 }
