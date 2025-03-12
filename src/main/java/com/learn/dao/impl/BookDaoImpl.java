45c7bc5b5330732b5728a6883ef063f6e15413dd
绿桶 <1325285734@qq.com>
2025-03-12 20:07:37 +0800

    feat:setter注入

--- src/main/java/com/learn/dao/impl/BookDaoImpl.java
+++ src/main/java/com/learn/dao/impl/BookDaoImpl.java
@@ -3,7 +3,18 @@ package com.learn.dao.impl;
 import com.learn.dao.BookDao;
 
 public class BookDaoImpl implements BookDao{
+    private String dataBaseName;
+    private String totalCount;
+
+    public void setDataBaseName(String dataBaseName) {
+        this.dataBaseName = dataBaseName;
+    }
+
+    public void setTotalCount(String totalCount) {
+        this.totalCount = totalCount;
+    }
+
     public void save() {
-        System.out.println("BookDaoImpl save...");
+        System.out.println("BookDaoImpl save... " + totalCount + " " + dataBaseName );
     }
 }
