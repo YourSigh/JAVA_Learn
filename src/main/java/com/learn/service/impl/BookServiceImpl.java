c89664d3a74cdb6b2cf6771ec957fb68fd800972
绿桶 <1325285734@qq.com>
2025-03-12 20:23:53 +0800

    feat:构造器注入

--- src/main/java/com/learn/service/impl/BookServiceImpl.java
+++ src/main/java/com/learn/service/impl/BookServiceImpl.java
@@ -1,12 +1,17 @@
 package com.learn.service.impl;
 
 import com.learn.dao.BookDao;
+import com.learn.dao.impl.BookDaoImpl;
 import com.learn.service.BookService;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 public class BookServiceImpl implements BookService, InitializingBean, DisposableBean {
     private BookDao bookDao;
+
+    public BookServiceImpl(BookDaoImpl bookDao) {
+        this.bookDao = bookDao;
+    }
 
     public void save() {
         System.out.println("BookServiceImpl save...");
