d0f6139b04cc6f58a06b5c6c5f7fbeeb0f5cf03b
绿桶 <1325285734@qq.com>
2025-03-25 21:27:49 +0800

    feat:AOP通知类型

--- src/main/java/com/learn/aop/MyAdvice.java
+++ src/main/java/com/learn/aop/MyAdvice.java
@@ -1,19 +1,42 @@
 package com.learn.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class MyAdvice {
 
-    @Pointcut("execution(void com.learn.service.impl.UsersServiceImpl.findAll())")
+    @Pointcut("execution(void com.learn.service.impl.UsersServiceImpl.test())")
+//    @Pointcut("execution(* *..*(..))")
     private void pt() {}
 
     @Before("pt()")
-    public void log() {
-        System.out.println("Logging...");
+    public void logBefore() {
+        System.out.println("Before advice running....");
+    }
+
+    @After("pt()")
+    public void logAfter() {
+        System.out.println("After advice running....");
+    }
+
+    @Around("pt()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("Around before advice running....");
+        Object obj = joinPoint.proceed();
+        System.out.println("Around after advice running....");
+        return obj;
+    }
+
+    @AfterReturning("pt()")
+    public void logAfterReturning() {
+        System.out.println("After return advice running....");
+    }
+
+    @AfterThrowing("pt()")
+    public void logAfterThrowing() {
+        System.out.println("After throw advice running....");
     }
 }
