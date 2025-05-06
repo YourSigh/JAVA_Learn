3b28e6bb9304f944963ca01e22bc2b149923101e
绿桶 <1325285734@qq.com>
2025-05-06 21:29:42 +0800

    feat:日期类型的参数接收

--- src/main/java/com/learn/controller/TestController.java
+++ src/main/java/com/learn/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.learn.controller;
 
 import com.learn.entity.User;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -24,5 +27,12 @@ public class TestController {
     public String jsonPost(@RequestBody User user){
         System.out.println(user.toString());
         return "{message:'post'}";
+    }
+
+    @RequestMapping("/dateTest")
+    @ResponseBody
+    public String dateTest(@DateTimeFormat(pattern = "yyyy-MM-dd") Date date){
+        System.out.println(date.toString());
+        return "{message:'dateTest'}";
     }
 }
